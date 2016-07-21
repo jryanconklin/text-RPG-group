c48754170fe1141792b5a95d3609d067e57d49bb
J. Ryan Conklin and Stephen Emery <emery.stephen@gmail.com>
2016-07-20 19:58:03 -0700

    Add function to print descriprions on movement.

--- js/scripts.js
+++ js/scripts.js
@@ -66,12 +66,95 @@ var attributeGen = function() {
   return 1 + Math.floor(Math.random() * 4);
 }
 
-// Description Start
-// xCord is an Adjective and yCord is a Verb
-// var desc = [ {
-//
-// }
-// ]
+var descriptions = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24"
+];
+
+
+Adventurer.prototype.spaceCheck = function() {
+  if (this.yCord === 0 && this.xCord === 0) {
+    console.log(descriptions[0]);
+    // this.items.push("The Earth Stone");
+  } else if (this.yCord === 1 && this.xCord === 0) {
+    console.log(descriptions[1]);
+  } else if (this.yCord === 2 && this.xCord === 0) {
+    console.log(descriptions[2]);
+  } else if (this.yCord === 3 && this.xCord === 0) {
+    console.log(descriptions[3]);
+  } else if (this.yCord === 4 && this.xCord === 0) {
+    console.log(descriptions[4]);
+    // this.items.push("The Water Stone");
+  } else if (this.yCord === 0 && this.xCord === 1) {
+    console.log(descriptions[5]);
+  } else if (this.yCord === 1 && this.xCord === 1) {
+    console.log(descriptions[6]);
+  } else if (this.yCord === 2 && this.xCord === 1) {
+    console.log(descriptions[7]);
+  } else if (this.yCord === 3 && this.xCord === 1) {
+    console.log(descriptions[8]);
+  } else if (this.yCord === 4 && this.xCord === 1) {
+    console.log(descriptions[9]);
+  } else if (this.yCord === 0 && this.xCord === 2) {
+    console.log(descriptions[10]);
+  } else if (this.yCord === 1 && this.xCord === 2) {
+    console.log(descriptions[11]);
+  } else if (this.yCord === 2 && this.xCord === 2) {
+    console.log(descriptions[12]);
+    // Starting
+  } else if (this.yCord === 3 && this.xCord === 2) {
+    console.log(descriptions[13]);
+  } else if (this.yCord === 4 && this.xCord === 2) {
+    console.log(descriptions[14]);
+  } else if (this.yCord === 0 && this.xCord === 3) {
+    console.log(descriptions[15]);
+  } else if (this.yCord === 1 && this.xCord === 3) {
+    console.log(descriptions[16]);
+  } else if (this.yCord === 2 && this.xCord === 3) {
+    console.log(descriptions[17]);
+  } else if (this.yCord === 3 && this.xCord === 3) {
+    console.log(descriptions[18]);
+  } else if (this.yCord === 4 && this.xCord === 3) {
+    console.log(descriptions[19]);
+  } else if (this.yCord === 0 && this.xCord === 4) {
+    console.log(descriptions[20]);
+    // this.items.push("The Sun Stone");
+  } else if (this.yCord === 1 && this.xCord === 4) {
+    console.log(descriptions[21]);
+  } else if (this.yCord === 2 && this.xCord === 4) {
+    console.log(descriptions[22]);
+  } else if (this.yCord === 3 && this.xCord === 4) {
+    console.log(descriptions[23]);
+  } else if (this.yCord === 4 && this.xCord === 4) {
+    console.log(descriptions[24]);
+    // forestLoop();
+  } else {
+    console.log("You're incredibly lost!")
+  }
+}
 
 
 // User Interface Logic
@@ -85,21 +168,25 @@ $(document).ready(function() {
 
   $("#north").click(function() {
     player.north();
+    player.spaceCheck();
     player.death();
   });
 
   $("#east").click(function() {
     player.east();
+    player.spaceCheck();
     player.death();
   });
 
   $("#south").click(function() {
     player.south();
+    player.spaceCheck();
     player.death();
   });
 
   $("#west").click(function() {
     player.west();
+    player.spaceCheck();
     player.death();
   });
 
